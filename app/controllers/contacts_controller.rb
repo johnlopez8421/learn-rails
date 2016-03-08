ffe80bc2ee378f6e56d87d71aed83813aa320455
John Lopez <john.lopez.8421@gmail.com>
2016-03-08 11:40:57 -0800

    troubleshoot mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,11 +6,12 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(secure_params)
     if @contact.valid?
-      Rails.logger.debug "DEBUG: #{@contact.email}"
+      Rails.logger.debug "DEBUG: ContactsController: #{@contact}"
       UserMailer.contact_email(@contact).deliver_now
       flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
     else
+      Rails.logger.debug "DEBUG: ContactsController: not valid"
       render :new
     end
   end
