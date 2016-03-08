2da88ab6419d31ef64849fbd75aa03c9bd3d4b08
John Lopez <john.lopez.8421@gmail.com>
2016-03-08 12:21:45 -0800

    troubleshoot mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
     Rails.logger.debug "DEBUG: UserMailer: env: " + Rails.env
-    Rails.logger.debug "DEBUG: UserMailer: owner_email: " + Rails.application.secrets.owner_email
-    Rails.logger.debug "DEBUG: UserMailer: contact: " + contact
+    #Rails.logger.debug "DEBUG: UserMailer: owner_email: " + Rails.application.secrets.owner_email
+    #Rails.logger.debug "DEBUG: UserMailer: contact: " + contact
     #mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
   end
 end
