9e97437df3fa058689ddfd9f57ebfda215155783
John Lopez <john.lopez.8421@gmail.com>
2016-03-08 13:06:22 -0800

    troubleshoot mailer

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,8 +5,8 @@ class Visitor
   validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
-    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
-    list_id = Rails.application.secrets.mailchimp_list_id
+    mailchimp = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API_KEY"])
+    list_id = ENV["MAILCHIMP_LIST_ID"]
     result = mailchimp.lists(list_id).members.create(
     body: {
       email_address: self.email,
