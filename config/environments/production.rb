64224c83fa7c5fe1d5c2632d4f817ec36d9a67d4
John Lopez <john.lopez.8421@gmail.com>
2016-03-08 11:20:36 -0800

    fix typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: immenseRails.application.secrets.domain_name,
+    domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
