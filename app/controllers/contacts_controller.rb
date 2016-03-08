461f100c51e1179507658e99726384dae23533fc
John Lopez <john.lopez.8421@gmail.com>
2016-03-08 12:30:42 -0800

    troubleshoot mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,8 @@ class ContactsController < ApplicationController
     @contact = Contact.new(secure_params)
     if @contact.valid?
       Rails.logger.debug "DEBUG: ContactsController: #{@contact}"
-      Rails.logger.debug "DEBUG: ContactsController: MC API KEY:" + ENV["MAILCHIMP_API_KEY"]
+      Rails.logger.debug "DEBUG: ContactsController: MC API KEY from ENV:" + ENV["MAILCHIMP_API_KEY"]
+      Rails.logger.debug "DEBUG: ContactsController: MC API KEY secrets:" + Rails.application.secrets.mailchimp_api_key
       Rails.logger.debug "DEBUG: ContactsController: contact.name: #{@contact.name}"
       UserMailer.contact_email(@contact).deliver_now
       #flash[:notice] = "Message sent from #{@contact.name}."
