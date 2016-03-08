1e25347554714219f67de68c7b83e0e276136e0d
John Lopez <john.lopez.8421@gmail.com>
2016-03-07 19:27:23 -0800

    fix secrets.yml typos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,13 +38,13 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: "immense-fortress-96456.herokuapp.com", #Rails.application.secrets.domain_name,
+    domain: immenseRails.application.secrets.domain_name,
     authentication: "plain",
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
 }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => "immense-fortress-96456.herokuapp.com" }#Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
